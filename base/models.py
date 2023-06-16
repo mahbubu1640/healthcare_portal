06c42f80be95e2edc0bfeb034ddad4b6920047d3
Mahbubur Rahman <104581346+mahbubu1640@users.noreply.github.com>
2023-06-16 17:11:54 +0530

    Update models.py

--- base/models.py
+++ base/models.py
@@ -9,63 +9,17 @@ class User(AbstractUser):
     Address=models.TextField(null=True,blank=True)
     is_patient = models.BooleanField(default=False)
     is_doctor = models.BooleanField(default=False)
-
-
-   
-
-
-
+    
     def __str__(self):
         return self.username
 
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    # def __str__(self):
-    #     return self.user.first_name
 
  
 
 class Doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    # def __str__(self):
-    #     return self.user.first_name
-   
-
-
-
-
-
-
-
-
-
-# class Doctor(models.Model):
-#     first_name=models.CharField(max_length=20,null=True,blank=True)
-#     last_name=models.CharField(max_length=20,null=True,blank=True)
-#     username=models.CharField(max_length=20,null=True,blank=True)
-#     img=models.ImageField(upload_to='myimages',null=True,blank=True)
-#     email=models.EmailField()
-#     password=models.CharField(max_length=20)
-#     Address=models.TextField(null=True,blank=True)
-#     is_doctor=models.BooleanField(default=False)
-
-#     def __str__(self):
-#         return self.username
-    
-
-# class Patient(models.Model):
-#     first_name=models.CharField(max_length=20,null=True,blank=True)
-#     last_name=models.CharField(max_length=20,null=True,blank=True)
-#     username=models.CharField(max_length=20,null=True,blank=True)
-#     img=models.ImageField(upload_to='myimages',null=True,blank=True)
-#     email=models.EmailField()
-#     password=models.CharField(max_length=20)
-#     is_Patient=models.BooleanField(default=False)
-#     Address=models.TextField(null=True,blank=True)
-
-#     def __str__(self):
-#         return self.username
-
 
 
 
