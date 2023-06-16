9208fd86af4f9f890796350c7551679d8a4350e5
Mahbubur Rahman <104581346+mahbubu1640@users.noreply.github.com>
2023-06-16 17:10:02 +0530

    Update views.py

--- base/views.py
+++ base/views.py
@@ -17,7 +17,6 @@ def Login(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        # print('---user',user)
         if user is not None:
             login(request, user)
             if user.is_patient:
