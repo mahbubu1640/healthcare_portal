8e19c795b8d7eaaea3eeb43895a07e6bd2162f5e
Mahbubur Rahman <104581346+mahbubu1640@users.noreply.github.com>
2023-10-15 05:29:45 +0530

    models.py

--- base/models.py
+++ base/models.py
@@ -13,11 +13,12 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+#Model for Patient
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
  
-
+#Model For Doctor
 class Doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
